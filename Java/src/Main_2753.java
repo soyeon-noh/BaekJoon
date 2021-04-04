f03987da53e727f6c605f742e70b56d74e93c56e
soyeon-noh <1010qwerty12@gmail.com>
2021-04-04 23:27:15 +0900

    4/4

---
+++ Java/src/Main_2753.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+public class Main_2753{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int sYear = 0;
+        sYear = scan.nextInt();
+        
+        if(sYear % 4 == 0){
+            if(sYear % 100 != 0 || sYear % 400 == 0 ){
+                System.out.println("1");
+            }else{
+                System.out.println("0");
+            }
+        }else {
+           System.out.println("0");
+        }
+        
+    }
+}
